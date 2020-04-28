7944bba633d640c760dd2c6bd4e3462bc77b03dc
anahi.gaetan <anahi.gaetan@globant.com>
2020-04-28 11:46:19 -0300

    Fix para instalar dependencia de SASS y se compile automático

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 //import './App.css';
-import "./styles/styles.css";
+import "./styles/styles.scss";
 
 function App() {
   return (
@@ -28,184 +28,190 @@ function App() {
           </ul>
         </nav>
       </header>
-      <section id="currencies" className="currencies">
-        <div className="currencies--container">
-          <div className="currencies--container--grid">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
+      <body>
+        <section id="currencies" className="currencies">
+          <div className="currencies--container">
+            <div className="currencies--container--grid">
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+            </div>
+            <div className="currencies--container--ad">ADD</div>
           </div>
-          <div className="currencies--container--ad">ADD</div>
-        </div>
-      </section>
+        </section>
 
-      <section id="news" className="news">
-        <div className="news--grid">
-          <article className="firstarticle">
-            <h1>
-              El Gobierno reconoció que más del 75% de los monotributistas está
-              severamente afectado por la crisis del coronavirus
-            </h1>
-            <figure>
-              <img
-                src="https://www.infobae.com/new-resizer/hDdKku6O08qefVOcSFhaJyji6D8=/999x561/filters:quality(100)/arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/WM7AU2BW5BFEBKMVXSAUT26PNA.jpg"
-                alt="article image"
-              />
-            </figure>
-            <button className="button--primary">Por autor</button>
-            <p>
-              Así lo precisó un informe técnico del Ministerio de Desarrollo
-              Productivo en base a información de la Administración Federal de
-              Ingresos Públicos (AFIP). “La mayoría de los trabajadores
-              independientes formales ha sufrido una caída notoria en su
-              actividad. El 75% de los monotributistas y el 71% de los autónomos
-              han visto recortada sus ventas y sus compras. Si bien esta
-              población es ciertamente menos vulnerable que los trabajadores
-              independientes informales, lejos está de estar ‘blindada’ ante la
-              pandemia”, manifestó el texto elaborado por el Gobierno.
-            </p>
-            <p>
-              De acuerdo a la información oficial, en la Argentina hay alrededor
-              de 5 millones de trabajadores independientes —que no son
-              asalariados en relación de dependencia— y que representan un 26%
-              del total del empleo del país. Sin embargo, su universo es muy
-              heterogéneo: por un lado, trabajadores independientes informales
-              (cuentapropistas) que no están inscriptos ante la AFIP y con
-              ingresos escasos; y por otro los formales..
-            </p>
-          </article>
-          <article>
-            <h1>
-              El Gobierno reconoció que más del 75% de los monotributistas está
-              severamente afectado por la crisis del coronavirus
-            </h1>
-            <figure>
-              <img
-                src="https://www.infobae.com/new-resizer/hDdKku6O08qefVOcSFhaJyji6D8=/999x561/filters:quality(100)/arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/WM7AU2BW5BFEBKMVXSAUT26PNA.jpg"
-                alt="article image"
-              />
-            </figure>
-            <button className="button--primary">Por autor</button>
-            <p>
-              Así lo precisó un informe técnico del Ministerio de Desarrollo
-              Productivo en base a información de la Administración Federal de
-              Ingresos Públicos (AFIP). “La mayoría de los trabajadores
-              independientes formales ha sufrido una caída notoria en su
-              actividad. El 75% de los monotributistas y el 71% de los autónomos
-              han visto recortada sus ventas y sus compras. Si bien esta
-              población es ciertamente menos vulnerable que los trabajadores
-              independientes informales, lejos está de estar ‘blindada’ ante la
-              pandemia”, manifestó el texto elaborado por el Gobierno.
-            </p>
-            <p>
-              De acuerdo a la información oficial, en la Argentina hay alrededor
-              de 5 millones de trabajadores independientes —que no son
-              asalariados en relación de dependencia— y que representan un 26%
-              del total del empleo del país. Sin embargo, su universo es muy
-              heterogéneo: por un lado, trabajadores independientes informales
-              (cuentapropistas) que no están inscriptos ante la AFIP y con
-              ingresos escasos; y por otro los formales..
-            </p>
-          </article>
-          <article>
-            <h1>
-              El Gobierno reconoció que más del 75% de los monotributistas está
-              severamente afectado por la crisis del coronavirus
-            </h1>
-            <figure>
-              <img
-                src="https://www.infobae.com/new-resizer/hDdKku6O08qefVOcSFhaJyji6D8=/999x561/filters:quality(100)/arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/WM7AU2BW5BFEBKMVXSAUT26PNA.jpg"
-                alt="article image"
-              />
-            </figure>
-            <button className="button--primary">Por autor</button>
-            <p>
-              Así lo precisó un informe técnico del Ministerio de Desarrollo
-              Productivo en base a información de la Administración Federal de
-              Ingresos Públicos (AFIP). “La mayoría de los trabajadores
-              independientes formales ha sufrido una caída notoria en su
-              actividad. El 75% de los monotributistas y el 71% de los autónomos
-              han visto recortada sus ventas y sus compras. Si bien esta
-              población es ciertamente menos vulnerable que los trabajadores
-              independientes informales, lejos está de estar ‘blindada’ ante la
-              pandemia”, manifestó el texto elaborado por el Gobierno.
-            </p>
-            <p>
-              De acuerdo a la información oficial, en la Argentina hay alrededor
-              de 5 millones de trabajadores independientes —que no son
-              asalariados en relación de dependencia— y que representan un 26%
-              del total del empleo del país. Sin embargo, su universo es muy
-              heterogéneo: por un lado, trabajadores independientes informales
-              (cuentapropistas) que no están inscriptos ante la AFIP y con
-              ingresos escasos; y por otro los formales..
-            </p>
-          </article>
-          <article>
-            <h1>
-              El Gobierno reconoció que más del 75% de los monotributistas está
-              severamente afectado por la crisis del coronavirus
-            </h1>
-            <figure>
-              <img
-                src="https://www.infobae.com/new-resizer/hDdKku6O08qefVOcSFhaJyji6D8=/999x561/filters:quality(100)/arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/WM7AU2BW5BFEBKMVXSAUT26PNA.jpg"
-                alt="article image"
-              />
-            </figure>
-            <button className="button--primary">Por autor</button>
+        <section id="news" className="news">
+          <div className="news--grid">
+            <article className="firstarticle">
+              <h1>
+                El Gobierno reconoció que más del 75% de los monotributistas
+                está severamente afectado por la crisis del coronavirus
+              </h1>
+              <figure>
+                <img
+                  src="https://www.infobae.com/new-resizer/hDdKku6O08qefVOcSFhaJyji6D8=/999x561/filters:quality(100)/arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/WM7AU2BW5BFEBKMVXSAUT26PNA.jpg"
+                  alt="article image"
+                />
+              </figure>
+              <button className="button--primary">Por autor</button>
+              <p>
+                Así lo precisó un informe técnico del Ministerio de Desarrollo
+                Productivo en base a información de la Administración Federal de
+                Ingresos Públicos (AFIP). “La mayoría de los trabajadores
+                independientes formales ha sufrido una caída notoria en su
+                actividad. El 75% de los monotributistas y el 71% de los
+                autónomos han visto recortada sus ventas y sus compras. Si bien
+                esta población es ciertamente menos vulnerable que los
+                trabajadores independientes informales, lejos está de estar
+                ‘blindada’ ante la pandemia”, manifestó el texto elaborado por
+                el Gobierno.
+              </p>
+              <p>
+                De acuerdo a la información oficial, en la Argentina hay
+                alrededor de 5 millones de trabajadores independientes —que no
+                son asalariados en relación de dependencia— y que representan un
+                26% del total del empleo del país. Sin embargo, su universo es
+                muy heterogéneo: por un lado, trabajadores independientes
+                informales (cuentapropistas) que no están inscriptos ante la
+                AFIP y con ingresos escasos; y por otro los formales..
+              </p>
+            </article>
+            <article>
+              <h1>
+                El Gobierno reconoció que más del 75% de los monotributistas
+                está severamente afectado por la crisis del coronavirus
+              </h1>
+              <figure>
+                <img
+                  src="https://www.infobae.com/new-resizer/hDdKku6O08qefVOcSFhaJyji6D8=/999x561/filters:quality(100)/arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/WM7AU2BW5BFEBKMVXSAUT26PNA.jpg"
+                  alt="article image"
+                />
+              </figure>
+              <button className="button--primary">Por autor</button>
+              <p>
+                Así lo precisó un informe técnico del Ministerio de Desarrollo
+                Productivo en base a información de la Administración Federal de
+                Ingresos Públicos (AFIP). “La mayoría de los trabajadores
+                independientes formales ha sufrido una caída notoria en su
+                actividad. El 75% de los monotributistas y el 71% de los
+                autónomos han visto recortada sus ventas y sus compras. Si bien
+                esta población es ciertamente menos vulnerable que los
+                trabajadores independientes informales, lejos está de estar
+                ‘blindada’ ante la pandemia”, manifestó el texto elaborado por
+                el Gobierno.
+              </p>
+              <p>
+                De acuerdo a la información oficial, en la Argentina hay
+                alrededor de 5 millones de trabajadores independientes —que no
+                son asalariados en relación de dependencia— y que representan un
+                26% del total del empleo del país. Sin embargo, su universo es
+                muy heterogéneo: por un lado, trabajadores independientes
+                informales (cuentapropistas) que no están inscriptos ante la
+                AFIP y con ingresos escasos; y por otro los formales..
+              </p>
+            </article>
+            <article>
+              <h1>
+                El Gobierno reconoció que más del 75% de los monotributistas
+                está severamente afectado por la crisis del coronavirus
+              </h1>
+              <figure>
+                <img
+                  src="https://www.infobae.com/new-resizer/hDdKku6O08qefVOcSFhaJyji6D8=/999x561/filters:quality(100)/arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/WM7AU2BW5BFEBKMVXSAUT26PNA.jpg"
+                  alt="article image"
+                />
+              </figure>
+              <button className="button--primary">Por autor</button>
+              <p>
+                Así lo precisó un informe técnico del Ministerio de Desarrollo
+                Productivo en base a información de la Administración Federal de
+                Ingresos Públicos (AFIP). “La mayoría de los trabajadores
+                independientes formales ha sufrido una caída notoria en su
+                actividad. El 75% de los monotributistas y el 71% de los
+                autónomos han visto recortada sus ventas y sus compras. Si bien
+                esta población es ciertamente menos vulnerable que los
+                trabajadores independientes informales, lejos está de estar
+                ‘blindada’ ante la pandemia”, manifestó el texto elaborado por
+                el Gobierno.
+              </p>
+              <p>
+                De acuerdo a la información oficial, en la Argentina hay
+                alrededor de 5 millones de trabajadores independientes —que no
+                son asalariados en relación de dependencia— y que representan un
+                26% del total del empleo del país. Sin embargo, su universo es
+                muy heterogéneo: por un lado, trabajadores independientes
+                informales (cuentapropistas) que no están inscriptos ante la
+                AFIP y con ingresos escasos; y por otro los formales..
+              </p>
+            </article>
+            <article>
+              <h1>
+                El Gobierno reconoció que más del 75% de los monotributistas
+                está severamente afectado por la crisis del coronavirus
+              </h1>
+              <figure>
+                <img
+                  src="https://www.infobae.com/new-resizer/hDdKku6O08qefVOcSFhaJyji6D8=/999x561/filters:quality(100)/arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/WM7AU2BW5BFEBKMVXSAUT26PNA.jpg"
+                  alt="article image"
+                />
+              </figure>
+              <button className="button--primary">Por autor</button>
 
-            <p>
-              Así lo precisó un informe técnico del Ministerio de Desarrollo
-              Productivo en base a información de la Administración Federal de
-              Ingresos Públicos (AFIP). “La mayoría de los trabajadores
-              independientes formales ha sufrido una caída notoria en su
-              actividad. El 75% de los monotributistas y el 71% de los autónomos
-              han visto recortada sus ventas y sus compras. Si bien esta
-              población es ciertamente menos vulnerable que los trabajadores
-              independientes informales, lejos está de estar ‘blindada’ ante la
-              pandemia”, manifestó el texto elaborado por el Gobierno.
-            </p>
-            <p>
-              De acuerdo a la información oficial, en la Argentina hay alrededor
-              de 5 millones de trabajadores independientes —que no son
-              asalariados en relación de dependencia— y que representan un 26%
-              del total del empleo del país. Sin embargo, su universo es muy
-              heterogéneo: por un lado, trabajadores independientes informales
-              (cuentapropistas) que no están inscriptos ante la AFIP y con
-              ingresos escasos; y por otro los formales..
-            </p>
-          </article>
-        </div>
-      </section>
+              <p>
+                Así lo precisó un informe técnico del Ministerio de Desarrollo
+                Productivo en base a información de la Administración Federal de
+                Ingresos Públicos (AFIP). “La mayoría de los trabajadores
+                independientes formales ha sufrido una caída notoria en su
+                actividad. El 75% de los monotributistas y el 71% de los
+                autónomos han visto recortada sus ventas y sus compras. Si bien
+                esta población es ciertamente menos vulnerable que los
+                trabajadores independientes informales, lejos está de estar
+                ‘blindada’ ante la pandemia”, manifestó el texto elaborado por
+                el Gobierno.
+              </p>
+              <p>
+                De acuerdo a la información oficial, en la Argentina hay
+                alrededor de 5 millones de trabajadores independientes —que no
+                son asalariados en relación de dependencia— y que representan un
+                26% del total del empleo del país. Sin embargo, su universo es
+                muy heterogéneo: por un lado, trabajadores independientes
+                informales (cuentapropistas) que no están inscriptos ante la
+                AFIP y con ingresos escasos; y por otro los formales..
+              </p>
+            </article>
+          </div>
+        </section>
 
-      <footer className="footer">
-        <ul id="socialmedia" className="footer--socialmedia">
-          <li>
-            <i class="fab fa-facebook-f"></i>
-          </li>
-          <li>
-            <i class="fab fa-twitter"></i>
-          </li>
-          <li>
-            <i class="fab fa-instagram"></i>
-          </li>
-          <li>
-            <i class="fab fa-youtube"></i>
-          </li>
-        </ul>
-        <div className="footer--info">
-          <figure>
-            <img
-              src="https://www.infobae.com/resources/assets/img/logo_infobae_naranja.svg"
-              alt="logo"
-              className="logo"
-            />
-          </figure>
-          <p>Todos Los Derechos Reservados © 2020 Infobae</p>
-        </div>
-      </footer>
+        <footer className="footer">
+          <ul id="socialmedia" className="footer--socialmedia">
+            <li>
+              <i class="fab fa-facebook-f"></i>
+            </li>
+            <li>
+              <i class="fab fa-twitter"></i>
+            </li>
+            <li>
+              <i class="fab fa-instagram"></i>
+            </li>
+            <li>
+              <i class="fab fa-youtube"></i>
+            </li>
+          </ul>
+          <div className="footer--info">
+            <figure>
+              <img
+                src="https://www.infobae.com/resources/assets/img/logo_infobae_naranja.svg"
+                alt="logo"
+                className="logo"
+              />
+            </figure>
+            <p>Todos Los Derechos Reservados © 2020 Infobae</p>
+          </div>
+        </footer>
+      </body>
     </div>
   );
 }
