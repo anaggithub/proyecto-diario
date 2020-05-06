2ac005239363ae320967b1c811976edcc837b4d3
Anahi <anahigaetan@hotmail.com>
2020-05-06 13:15:53 -0300

    Agregando layout y page home

---
+++ src/layouts/default-layout.js
@@ -0,0 +1,20 @@
+import React from "react"
+import "./../styles/styles.scss";
+import Header from "./../components/header/header";
+import DollarRate from "./../components/dollar-rate/dollar-rate";
+import News from "./../components/news/news";
+import Footer from "./../components/footer/footer";
+
+const DefaultLayout = () => {
+
+    return (
+        <div className="container">
+            <Header />
+            <DollarRate />
+            <News />
+            <Footer />
+        </div>
+    );
+}
+
+export default DefaultLayout;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,11 @@
 import React from "react"
 import "./../styles/styles.scss";
-import Header from "./../components/header/header";
-import DollarRate from "./../components/dollar-rate/dollar-rate";
-import News from "./../components/news/news";
-import Footer from "./../components/footer/footer";
+import DefaultLayout from "./../layouts/default-layout"
 
 const Home = () => {
 
     return (
-        <div className="container">
-            <Header />
-            <DollarRate />
-            <News />
-            <Footer />
-        </div>
+        <DefaultLayout />
     );
 }
 
