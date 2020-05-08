5daf7b9ddc27b4deffa2dc4cfcb003807af928e9
Anahi <anahigaetan@hotmail.com>
2020-05-08 15:30:30 -0300

    Reorganizando header y nav

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import SearchContainer from "../search-container"
-import Nav from "./nav"
 import APITitle from "../apititle"
 
 const Header = () => {
 
+  const [isNavOpen, setNavOpen] = useState(false);
+  const iconClass = isNavOpen ? "fas fa-times" : "fas fa-bars";
+  const navDisplay = isNavOpen ? "block" : "none";
+
+  const navItems = ["Últimas noticias",
+    "Política",
+    "Sociedad",
+    "Deportes",
+    "Techno",
+    "Docs",
+    "Campo",
+    "Tendencias",
+    "Vidriera",
+    "Personajes"
+  ]
+
   return (
     <header className="header">
-      <Nav />
+      <nav className="header--nav" onClick={() => setNavOpen(!isNavOpen)}>
+        <i className={`header--nav--icon ${iconClass}`}></i>
+        <ul className="header--nav--list" style={{ display: navDisplay }}>
+          {navItems.map(elem => {
+            return <li
+              className="header--nav--list--element"
+              key={elem + 1}>
+              {elem}
+            </li>
+          })
+          }
+        </ul>
+      </nav>
       <APITitle />
       <SearchContainer />
     </header>
